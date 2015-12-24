ce17c001a915d56dd51c0833a326f19174707065
Well <309635368@qq.com>
2015-12-24 15:23:13 +1300

    removed old code

--- app/actions/todoActions.js
+++ app/actions/todoActions.js
@@ -1,23 +1,3 @@
 import alt from '../libs/alt';
 
 export default alt.generateActions('addItem', 'removeItem');
-
-// import AppDispatcher from '../dispatcher/AppDispatcher';
-// import appConstants from '../constants/appConstants';
-//
-// const todoActions = {
-//   addItem(item){
-//     AppDispatcher.handleAction({
-//       actionType: appConstants.ADD_ITEM,
-//       data: item
-//     });
-//   },
-//   removeItem(index){
-//     AppDispatcher.handleAction({
-//       actionType: appConstants.REMOVE_ITEM,
-//       data: index
-//     })
-//   }
-// };
-//
-// export default todoActions;
